1c331a297700116a3025691e44f83bab04ea9ac3
Tony Medhat <tonymedhat5@tutanota.com>
2026-04-16 11:35:38 +0200

    bench: implement multi-metric optimization

--- benchmarks/src/main/kotlin/com/google/samples/apps/nowinandroid/Generator/BookmarkForYouArticle_Bench.kt
+++ benchmarks/src/main/kotlin/com/google/samples/apps/nowinandroid/Generator/BookmarkForYouArticle_Bench.kt
@@ -6,7 +6,6 @@ import androidx.benchmark.macro.ExperimentalMetricApi
 import androidx.benchmark.macro.FrameTimingMetric
 import androidx.benchmark.macro.MemoryUsageMetric
 import androidx.benchmark.macro.StartupMode
-import androidx.benchmark.macro.StartupTimingMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.uiautomator.By
@@ -15,22 +14,22 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
+@OptIn(ExperimentalMetricApi::class)
 @RunWith(AndroidJUnit4::class)
-class FollowInterestTopic_FT {
+class BookmarkForYouArticle_Bench {
     @get:Rule
     val benchmarkRule = MacrobenchmarkRule()
 
     @Test
-    fun followInterestTopicFT() = benchmarkRule.measureRepeated(
+    fun bookmarkForYouArticle() = benchmarkRule.measureRepeated(
         packageName = "com.google.samples.apps.nowinandroid.demo",
-        metrics = listOf(FrameTimingMetric()),
+        metrics = listOf(MemoryUsageMetric(MemoryUsageMetric.Mode.Max), FrameTimingMetric()),
         startupMode = StartupMode.WARM,
         compilationMode = CompilationMode.Partial(
             baselineProfileMode = BaselineProfileMode.Disable,
-            warmupIterations = 3,
+            warmupIterations = 1,
         ),
-        iterations = 10,
+        iterations = 5,
         setupBlock = {
             pressHome()
             startActivityAndWait()
@@ -42,19 +41,22 @@ class FollowInterestTopic_FT {
         device.click(((500.0f / 1000.0f) * device.displayWidth).toInt(), ((547.0f / 1000.0f) * device.displayHeight).toInt())
         device.waitForIdle()
         Thread.sleep(1000L)
-        device.swipe(((500.0f / 1000.0f) * device.displayWidth).toInt(), ((797.0f / 1000.0f) * device.displayHeight).toInt(), ((525.0f / 1000.0f) * device.displayWidth).toInt(), ((329.0f / 1000.0f) * device.displayHeight).toInt(), 20)
-        device.waitForIdle()
-        Thread.sleep(1000L)
-        device.click(((738.0f / 1000.0f) * device.displayWidth).toInt(), ((444.0f / 1000.0f) * device.displayHeight).toInt())
-        device.waitForIdle()
-        Thread.sleep(1000L)
-        device.click(((739.0f / 1000.0f) * device.displayWidth).toInt(), ((444.0f / 1000.0f) * device.displayHeight).toInt())
-        device.waitForIdle()
-        Thread.sleep(1000L)
-        device.click(((738.0f / 1000.0f) * device.displayWidth).toInt(), ((445.0f / 1000.0f) * device.displayHeight).toInt())
+        device.click(((500.0f / 1000.0f) * device.displayWidth).toInt(), ((537.0f / 1000.0f) * device.displayHeight).toInt())
         device.waitForIdle()
         Thread.sleep(1000L)
         device.click(((500.0f / 1000.0f) * device.displayWidth).toInt(), ((537.0f / 1000.0f) * device.displayHeight).toInt())
+        device.waitForIdle()
+        Thread.sleep(1000L)
+        device.click(((738.0f / 1000.0f) * device.displayWidth).toInt(), ((295.0f / 1000.0f) * device.displayHeight).toInt())
+        device.waitForIdle()
+        Thread.sleep(1000L)
+        device.click(((500.0f / 1000.0f) * device.displayWidth).toInt(), ((537.0f / 1000.0f) * device.displayHeight).toInt())
+        device.waitForIdle()
+        Thread.sleep(1000L)
+        device.swipe(((500.0f / 1000.0f) * device.displayWidth).toInt(), ((797.0f / 1000.0f) * device.displayHeight).toInt(), ((525.0f / 1000.0f) * device.displayWidth).toInt(), ((289.0f / 1000.0f) * device.displayHeight).toInt(), 20)
+        device.waitForIdle()
+        Thread.sleep(1000L)
+        device.click(((843.0f / 1000.0f) * device.displayWidth).toInt(), ((625.0f / 1000.0f) * device.displayHeight).toInt())
         device.waitForIdle()
         Thread.sleep(1000L)
         // ACTION: COMPLETE
--- benchmarks/src/main/kotlin/com/google/samples/apps/nowinandroid/Generator/FollowInterestTopic_Bench.kt
+++ benchmarks/src/main/kotlin/com/google/samples/apps/nowinandroid/Generator/FollowInterestTopic_Bench.kt
@@ -6,7 +6,6 @@ import androidx.benchmark.macro.ExperimentalMetricApi
 import androidx.benchmark.macro.FrameTimingMetric
 import androidx.benchmark.macro.MemoryUsageMetric
 import androidx.benchmark.macro.StartupMode
-import androidx.benchmark.macro.StartupTimingMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.uiautomator.By
@@ -17,20 +16,20 @@ import org.junit.runner.RunWith
 
 @OptIn(ExperimentalMetricApi::class)
 @RunWith(AndroidJUnit4::class)
-class BookmarkForYouArticle_MU {
+class FollowInterestTopic_Bench {
     @get:Rule
     val benchmarkRule = MacrobenchmarkRule()
 
     @Test
-    fun bookmarkForYouArticleMEM() = benchmarkRule.measureRepeated(
+    fun followInterestTopic() = benchmarkRule.measureRepeated(
         packageName = "com.google.samples.apps.nowinandroid.demo",
-        metrics = listOf(MemoryUsageMetric(MemoryUsageMetric.Mode.Max)),
+        metrics = listOf(MemoryUsageMetric(MemoryUsageMetric.Mode.Max), FrameTimingMetric()),
         startupMode = StartupMode.WARM,
         compilationMode = CompilationMode.Partial(
             baselineProfileMode = BaselineProfileMode.Disable,
-            warmupIterations = 3,
+            warmupIterations = 1,
         ),
-        iterations = 10,
+        iterations = 5,
         setupBlock = {
             pressHome()
             startActivityAndWait()
@@ -42,22 +41,19 @@ class BookmarkForYouArticle_MU {
         device.click(((500.0f / 1000.0f) * device.displayWidth).toInt(), ((547.0f / 1000.0f) * device.displayHeight).toInt())
         device.waitForIdle()
         Thread.sleep(1000L)
-        device.click(((500.0f / 1000.0f) * device.displayWidth).toInt(), ((537.0f / 1000.0f) * device.displayHeight).toInt())
+        device.swipe(((500.0f / 1000.0f) * device.displayWidth).toInt(), ((797.0f / 1000.0f) * device.displayHeight).toInt(), ((525.0f / 1000.0f) * device.displayWidth).toInt(), ((329.0f / 1000.0f) * device.displayHeight).toInt(), 20)
+        device.waitForIdle()
+        Thread.sleep(1000L)
+        device.click(((738.0f / 1000.0f) * device.displayWidth).toInt(), ((444.0f / 1000.0f) * device.displayHeight).toInt())
+        device.waitForIdle()
+        Thread.sleep(1000L)
+        device.click(((739.0f / 1000.0f) * device.displayWidth).toInt(), ((444.0f / 1000.0f) * device.displayHeight).toInt())
+        device.waitForIdle()
+        Thread.sleep(1000L)
+        device.click(((738.0f / 1000.0f) * device.displayWidth).toInt(), ((445.0f / 1000.0f) * device.displayHeight).toInt())
         device.waitForIdle()
         Thread.sleep(1000L)
         device.click(((500.0f / 1000.0f) * device.displayWidth).toInt(), ((537.0f / 1000.0f) * device.displayHeight).toInt())
-        device.waitForIdle()
-        Thread.sleep(1000L)
-        device.click(((738.0f / 1000.0f) * device.displayWidth).toInt(), ((295.0f / 1000.0f) * device.displayHeight).toInt())
-        device.waitForIdle()
-        Thread.sleep(1000L)
-        device.click(((500.0f / 1000.0f) * device.displayWidth).toInt(), ((537.0f / 1000.0f) * device.displayHeight).toInt())
-        device.waitForIdle()
-        Thread.sleep(1000L)
-        device.swipe(((500.0f / 1000.0f) * device.displayWidth).toInt(), ((797.0f / 1000.0f) * device.displayHeight).toInt(), ((525.0f / 1000.0f) * device.displayWidth).toInt(), ((289.0f / 1000.0f) * device.displayHeight).toInt(), 20)
-        device.waitForIdle()
-        Thread.sleep(1000L)
-        device.click(((843.0f / 1000.0f) * device.displayWidth).toInt(), ((625.0f / 1000.0f) * device.displayHeight).toInt())
         device.waitForIdle()
         Thread.sleep(1000L)
         // ACTION: COMPLETE
